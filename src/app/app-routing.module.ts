679dcf6fa82d7b4c059f1768108be43c193c6df8
duc <hohuynhduca7bh123@gmail.com>
2020-08-18 14:22:53 +0700

    fix something

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'Login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'Home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'listMusic', loadChildren: () => import('./pages/list-music/list-music.module').then(m => m.ListMusicModule) }];
+const routes: Routes = [
+  { path: 'Login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'Home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'listMusic', loadChildren: () => import('./pages/list-music/list-music.module').then(m => m.ListMusicModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
