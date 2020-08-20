ee2544fa8da08e0ca58bf7c37caa04ef6d1b40d8
duc <hohuynhduca7bh123@gmail.com>
2020-08-20 15:22:24 +0700

    Toang rooi ong giao a

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import * as firebase from 'firebase'
@@ -7,12 +8,16 @@ import * as firebase from 'firebase'
 })
 export class AuthService {
   public user: firebase.User=null;
-  constructor(private auth:AngularFireAuth) { }
+  constructor(
+    private auth:AngularFireAuth,
+    public router: Router,
+    ) { }
   public async logingg(){
     try{
     let provider=new firebase.auth.GoogleAuthProvider();
     await this.auth.signInWithPopup(provider);
     this.user = await this.auth.currentUser;
+    this.router.navigate(['home']);
   }catch(err){
     console.error(err);
   }
@@ -21,5 +26,5 @@ export class AuthService {
     await this.auth.signOut();
     this.user=null;
   }
- 
+
 }
